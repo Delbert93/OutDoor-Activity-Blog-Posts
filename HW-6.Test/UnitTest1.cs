c8619fafcdb401e80378d8a1dce9ad6c3487d75f
McKinnin <lloydmckinnin@gmail.com>
2020-10-06 16:12:36 -0600

    added my unit test project

---
+++ HW-6.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace HW_6.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetBlogPostAsync()
+        {
+            int blogId = 1;
+            Assert.Fail();
+        }
+    }
+}
