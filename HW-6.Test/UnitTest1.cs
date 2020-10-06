96dde9a1b83655e19f7356277f0a326605e5423a
McKinnin <lloydmckinnin@gmail.com>
2020-10-06 16:42:45 -0600

    finding some help on getting tests set up for the database. Found help at https://entityframeworkcore.com/providers-inmemory and https://www.infoworld.com/article/3234531/testing-with-the-inmemory-provider-in-entity-framework-core.html

--- HW-6.Test/UnitTest1.cs
+++ HW-6.Test/UnitTest1.cs
@@ -1,12 +1,17 @@
+using HW_6.Data;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 
 namespace HW_6.Test
 {
     public class Tests
-    {
+    {     
         [SetUp]
         public void Setup()
         {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "Test")
+            .Options;
         }
 
         [Test]
